6e278bcbd513fd12e8951d5478926ea3b11bf9b0
amitthanki <amit.thanki04@gmail.com>
2018-07-15 21:38:06 +0530

    lll

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {AgGridModule} from 'ag-grid-angular/main';
 
 import { AppComponent } from './app.component';
+import { TableComponentComponent } from './table-component.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponentComponent
   ],
   imports: [
     BrowserModule
